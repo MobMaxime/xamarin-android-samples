2bbb8316aceb46454b018bc24d50b444bb2cb50b
dharmik patel <dharmikisback@gmail.com>
2015-08-04 19:41:16 +0530

    Custom Calendar
    Custom Calendar

---
+++ Calendar_Android/Calendar_Android/Activity/CalendarAdapter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Android.Widget;
+using Android.Content;
+using Java.Util;
+using Android.Views;
+
+namespace Calendar_Android
+{
+	public class CalendarAdapter : BaseAdapter
+	{
+		int FIRST_DAY_OF_WEEK = 0; // Sunday = 0, Monday = 1
+
+
+		private Context mContext;
+
+		private Calendar month;
+		private Calendar selectedDate;
+		private ArrayList items;
+		public String[] days;
+
+		public CalendarAdapter(Context c, Calendar monthCalendar) {
+
+			month = monthCalendar;
+			selectedDate = (Calendar)monthCalendar.Clone();
+			mContext = c;
+			month.Set(Calendar.DayOfMonth, 1);
+			this.items = new ArrayList();
+			refreshDays();
+		}
+
+		public void setItems(ArrayList items) {
+			for(int i = 0;i != items.Size();i++){
+				//if(items.li == 1) {
+					items.Set(i, "0" + items.Get(i));
+				//}
+			}
+			this.items = items;
+		}
+
+		public override Java.Lang.Object GetItem (int position)
+		{
+			return null;
+		}
+		public override long GetItemId (int position)
+		{
+			return 0;
+		}
+		public override int Count {
+			get {
+				return days.Length;
+			}
+		}
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			View view = convertView;
+			TextView dayView;
+			if (view == null) // no view to re-use, create new
+				view = LayoutInflater.From(mContext).Inflate(Resource.Layout.calendar_item, null);
+
+			dayView = (TextView)view.FindViewById(Resource.Id.date);
+
+
+			// disable empty days from the beginning
+			if(days[position].Equals("")) {
+				dayView.Clickable = false;
+				dayView.Focusable = false;
+			}else {
+				// mark current day as focused
+				if(month.Get(Calendar.Year)== selectedDate.Get(Calendar.Year) && month.Get(Calendar.Month)== selectedDate.Get(Calendar.Month) && days[position].Equals(""+selectedDate.Get(Calendar.DayOfMonth))) {
+					view.SetBackgroundColor (Android.Graphics.Color.Blue);
+				}
+				else {
+					//v.setBackgroundResource(R.drawable.list_item_background);
+				}
+			}
+
+			dayView.Text = days [position];
+			String date = days[position];
+
+			if(date.Length == 1) {
+				date = "0"+ date;
+			}
+			String monthStr = ""+(month.Get(Calendar.Month)+1);
+			if(monthStr.Length == 1) {
+				monthStr = "0"+monthStr;
+			}
+
+			ImageView iw = (ImageView)view.FindViewById(Resource.Id.date_icon);
+			if(date.Length>0 && items!=null && items.Contains(date)) {        	
+				iw.Visibility = ViewStates.Visible;
+			}
+			else {
+				iw.Visibility = ViewStates.Invisible;
+			}
+
+			return view;
+		}
+
+		public void refreshDays()
+		{
+			items.Clear ();
+
+			int lastDay = month.GetActualMaximum (Calendar.DayOfMonth);
+			int firstDay = month.Get (Calendar.DayOfWeek);
+
+			// figure size of the array
+			if(firstDay==1){
+				days = new string[lastDay+(FIRST_DAY_OF_WEEK*6)];
+			}
+			else {
+				days = new String[lastDay+firstDay-(FIRST_DAY_OF_WEEK+1)];
+			}
+			int j=FIRST_DAY_OF_WEEK;
+
+			// populate empty days before first real day
+			if(firstDay>1) {
+				for(j=0;j<firstDay-FIRST_DAY_OF_WEEK;j++) {
+					days[j] = "";
+				}
+			}
+			else {
+				for(j=0;j<FIRST_DAY_OF_WEEK*6;j++) {
+					days[j] = "";
+				}
+				j=FIRST_DAY_OF_WEEK*6+1; // sunday => 1, monday => 7
+			}
+
+			// populate days
+			int dayNumber = 1;
+			for(int i=j-1;i<days.Length;i++) {
+				days[i] = ""+dayNumber;
+				dayNumber++;
+			}
+		}
+	}
+}
+
---
+++ Calendar_Android/Calendar_Android/Activity/MainActivity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Util;
+using Java.Lang;
+
+
+
+namespace Calendar_Android
+{
+	[Activity (Label = "Calendar_Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+		public Calendar month;
+		public CalendarAdapter adapter;
+		public ArrayList items;
+		public Handler handler;
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			month = Calendar.Instance;
+
+			//Static date set
+			System.String date = "2015-07-31";
+			char[] splitchar = { '-' };
+			System.String[] dateArr = date.Split(splitchar); // date format is yyyy-mm-dd
+
+
+
+			//month.set(Integer.parseInt(dateArr[0]), Integer.parseInt(dateArr[1]), Integer.parseInt(dateArr[2]));
+			month.Set(Int16.Parse(dateArr[0]), Int16.Parse(dateArr[1]),Int16.Parse(dateArr[2]));
+
+			items = new ArrayList();
+			adapter = new CalendarAdapter(this, month);
+
+			date_date ();
+
+			GridView gridview = (GridView)FindViewById (Resource.Id.gridview);
+			gridview.SetAdapter(adapter);
+			gridview.ItemClick += grid_click;
+
+			TextView title  = (TextView) FindViewById(Resource.Id.title);
+			title.Text = Android.Text.Format.DateFormat.Format("MMMM yyyy",month);
+
+			TextView previous = (TextView) FindViewById (Resource.Id.previous);
+			previous.Click += previous_click; 
+
+			TextView next = (TextView) FindViewById (Resource.Id.next);
+			next.Click += next_click; 
+
+		}
+
+		public void previous_click(object sender, EventArgs e){
+			if(month.Get(Calendar.Month) == month.GetActualMinimum(Calendar.Month)) {				
+				month.Set((month.Get(Calendar.Year)-1),month.GetActualMaximum(Calendar.Month),1);
+			} else {
+				month.Set(Calendar.Month,month.Get(Calendar.Month)-1);
+			}
+			refreshCalendar();
+		}
+
+		public void next_click(object sender, EventArgs e){
+			if(month.Get(Calendar.Month)== month.GetActualMaximum(Calendar.Month)) {				
+				month.Set((month.Get(Calendar.Year)+1),month.GetActualMinimum(Calendar.Month),1);
+			} else {
+				month.Set(Calendar.Month,month.Get(Calendar.Month)+1);
+			}
+			refreshCalendar();
+		}
+
+		public void grid_click(object sender, AdapterView.ItemClickEventArgs e){
+
+			TextView date = (TextView)e.View.FindViewById(Resource.Id.date);
+			System.String day = date.Text.ToString ();
+
+			if(day.Length == 1) {
+				day = "0"+day;
+			}
+			Toast.MakeText (this, "Date --->" + Android.Text.Format.DateFormat.Format("yyyy-MM", month)+"-"+day, ToastLength.Short).Show ();
+		}
+
+		public void refreshCalendar()
+		{
+			TextView title = (TextView)FindViewById (Resource.Id.title);
+
+			adapter.refreshDays();
+			adapter.NotifyDataSetChanged();
+			date_date();
+			title.Text = Android.Text.Format.DateFormat.Format("MMMM yyyy",month);
+		}
+		public void date_date(){
+			items.Clear ();
+			// format random values. You can implement a dedicated class to provide real values
+			for(int i=0;i<31;i++) {
+				System.Random r = new System.Random();
+
+				int a = r.Next (10);
+				if (a > 6) {
+					items.Add (i.ToString ());
+				} else {
+					Console.WriteLine ("Ok");
+				}
+
+			}
+
+			adapter.setItems(items);
+			adapter.NotifyDataSetChanged ();
+		}
+
+
+	}
+}
+
+
